49199a37b620ee3d518df515f8fee0a904d69e98
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-02-11 20:04:33 -0300

    add get_balance, transfer and check_funds methods

--- budget.py
+++ budget.py
@@ -7,11 +7,27 @@ class Category:
         self.ledger.append({"amount": amount, "description": description})
 
     def withdraw(self, amount, description=""):
-        if self.check_balance(amount):
+        if self.check_funds(amount):
             self.ledger.append({"amount": amount, "description": description})
+            return True
+        return False
 
-        return self.check_balance(amount)
+    def get_balance(self):
+        balance = 0
+        for transaction in self.ledger:
+            balance += transaction["amount"]
+        return balance
+
+    def transfer(self, amount, another_category):
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {another_category.name}")
+            another_category.deposit(amount, f"Transfer from {self.name}")
+            return True
+        return False
+
+    def check_funds(self, amount):
+        return amount <= self.get_balance()
 
 
-# def create_spend_chart(categories):
-# pass
+def create_spend_chart(categories):
+    pass
