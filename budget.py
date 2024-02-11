8b75efc1e57baff3f5c72f5e29222306136d3860
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-02-11 18:53:23 -0300

    add deposit and withdraw methods

--- budget.py
+++ budget.py
@@ -1,7 +1,17 @@
 class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self, amount, description=""):
+        if self.check_balance(amount):
+            self.ledger.append({"amount": amount, "description": description})
+
+        return self.check_balance(amount)
 
 
-
-
-
-def create_spend_chart(categories):
+# def create_spend_chart(categories):
+# pass
